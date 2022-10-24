fe1469e6e439b18d5b30f5ab6645912cdd3260d5
Megabd <bath@itu.dk>
2022-10-24 16:43:16 +0200

    stuff

---
+++ Server/server.go
@@ -0,0 +1,10 @@
+package main
+
+//gRPC "github.com/Megabd/DStests/proto"
+
+type Server struct {
+	// an interface that the server needs to have
+	//gRPC.UnimplementedTemplateServer
+
+	// here you can impliment other fields that you want
+}
